5c77e8dc3edf0ee91572b3cafd84c3e8bf191787
Pratik Pandey <pratikproths04@gmail.com>
2017-04-04 02:11:00 -0700

    added skyscanner api code

--- app.js
+++ app.js
@@ -15,8 +15,64 @@ var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var errorHandler = require('errorhandler');
-
 var app = express();
+
+//Put your skyscanner api key here
+var api_key = "sa959583952162241927741272970024";
+
+var baseURI = 'http://partners.api.skyscanner.net/apiservices/carhire/'; 
+var browse = baseURI + 'liveprices/v2/';
+
+app.get('/getPrice', function (req, res) {
+	var market = (req.param("country")) ? req.param("country") : 'US';
+	console.log("market: "+market);
+	var currency = (req.param("currency")) ? req.param("currency") : 'USD';
+	console.log("currency: "+currency);
+	var locale = (req.param("language")) ? req.param("language") : 'en-us';
+	console.log("locale: "+locale);
+	var pickupplace = (req.param("pickupplace"));
+	console.log("pickupplace: "+pickupplace);
+	var dropoffplace = req.param("dropoffplace");
+	console.log("dropoffplace: "+dropoffplace);
+	var pickupdatetime = req.param("pickupdatetime");
+	console.log("pickupdatetime: "+pickupdatetime);
+	/*var dropoffdatetime = req.param("dropoffdatetime");
+	console.log("dropoffdatetime: "+dropoffdatetime);
+	var driverage = req.param("driverage");
+	console.log("driverage: "+driverage);*/
+	
+	var options = {
+	  host: browse,
+	  path: '/'+market+'/'+currency+'/'+locale+'/'+pickupplace+'/'
+	  		+dropoffplace+'/'+pickupdatetime
+	};
+	
+	var req = http.get(options, function(res) {
+	  console.log('STATUS: ' + res.statusCode);
+	  console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+	  // Buffer the body entirely for processing as a whole.
+	  var bodyChunks = [];
+	  res.on('data', function(chunk) {
+	    // You can process streamed parts here...
+		console.log('chunk: ' + chunk);
+	    bodyChunks.push(chunk);
+	  }).on('end', function() {
+		console.log('bodyChunks: ' + bodyChunks);
+	    var body = Buffer.concat(bodyChunks);
+	    console.log('BODY: ' + body);
+	    if(body!= null){
+	    	res.send({status: "success", "data":body});
+	    }
+	    res.send({status: "fail", "data":body});
+	    // ...and/or process the entire body here.
+	  })
+	});
+
+	req.on('error', function(e) {
+	  console.log('ERROR: ' + e.message);
+	});
+});
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -31,7 +87,7 @@ app.set('view engine', 'ejs');
 
 // development only
 if (app.get('env') === 'development') {
-	  app.use(errorHandler())
+	  app.use(errorHandler());
 }
 
 app.get('/', routes.index);
